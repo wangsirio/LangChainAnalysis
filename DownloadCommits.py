ea2435d6995147ebe6206a22e22820146fb37d75
wang_siri <wangshirui122333@163.com>
2025-01-14 22:46:14 +0800

    完成了将commit下载到本地的操作

---
+++ DownloadCommits.py
@@ -0,0 +1,25 @@
+import json
+import os
+import requests
+GithubToken = os.environ.get('GITHUB_TOKEN')
+if GithubToken is None:
+    raise ValueError("The environment variable GITHUB_TOKEN is not set.")
+headers = {'Authorization': f'token {GithubToken}'}
+history=[]
+flag=1
+i=0
+url = "https://api.github.com/repos/langchain-ai/langchain/commits"
+while(flag and i<2):
+    params = {'per_page': '100',
+              'page': {i}}
+    response = requests.get(url,verify = False,headers=headers,params = params)
+    if response.status_code == 200:
+        commits = response.json()
+        history.append(commits)
+        print(f'page {i} finished')
+        i = i + 1
+    else:
+        flag=0
+        print(f"Failed to retrieve commits: {response.text}")
+with open(f'./commits2','w',encoding = 'utf-8')as file:
+    json.dump(history,file, ensure_ascii=False, indent=4)
